31bdc73c143d9bd103e9b08b9922c75be291d600
Alptekin Ozturk (BilgeAdam Ogrenci) <alptekin.ozturk@baogrenci.com>
2023-11-15 15:19:50 +0300

    next commit

---
+++ Models/CommentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ViewComponent_MVC.Models
+{
+    public class CommentViewModel
+    {
+        public string CommentBy { get; set; }
+        public string Comment { get; set; }
+        public DateTime CommentTime { get; set; }
+    }
+}
---
+++ ViewComponents/CommentViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponent_MVC.Models;
+
+namespace ViewComponent_MVC.ViewComponents
+{
+    public class CommentViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = new List<CommentViewModel>
+            {
+                new CommentViewModel
+                {
+                    CommentBy = "Fatih",
+                    Comment = "Bugün hiç uykum yok. Enerjim top seviyede!",
+                    CommentTime = DateTime.Now
+                },
+                new CommentViewModel
+                {
+                    CommentBy = "Gürkan",
+                    Comment = "Bugün çok güzel bir gün.",
+                    CommentTime = DateTime.Now.AddDays(-2)
+                },
+                new CommentViewModel
+                {
+                    CommentBy = "Hüseyin",
+                    Comment = "Bugün kendimi çok yalnız hissediyorum",
+                    CommentTime = DateTime.Now.AddDays(-7)
+                }
+            };
+
+            return View(model);
+        }
+    }
+}
